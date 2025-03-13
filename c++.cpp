169db740f45daef0595169b42bbbefbff061d3a5
babe-baby <sokmeyling04@gmail.com>
2025-03-13 11:32:14 +0700

    my first commit

---
+++ c++.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+using namespace std;
+void sayhello();
+int main()
+{
+    sayhello();
+//在显示器上输出显示一行字符串
+    cout<<"This is my first c++ program!"<<endl;
+}
+//函数定义
+void sayhello()
+{
+    cout<<"Hello!"<<endl;
+}
